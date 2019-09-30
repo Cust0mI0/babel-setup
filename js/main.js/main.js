4fb9ce743bb3224c6fe6ae9abb28964c18f3fa01
Sebastian Tramper <tramper.s@outlook.com>
2019-09-30 22:14:15 +0200

    babel setup

---
+++ js/main.js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var main = function main(name) {
+    _classCallCheck(this, main);
+
+    this.name = name;
+};
---
+++ src/main.js
@@ -0,0 +1,5 @@
+class main{
+    constructor(name) {
+        this.name = name;
+    } 
+}
